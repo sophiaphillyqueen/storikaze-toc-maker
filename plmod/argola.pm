0fff3f5374eebb18b2fdefe1e9d56cb1f3efa6a5
Sophia Elizabeth Shapira <Sophia.Shapira@gmail.com>
2015-08-18 02:36:02 -0400

    Updated the argola perl module

--- plmod/argola.pm
+++ plmod/argola.pm
@@ -5,6 +5,8 @@ my @argbuft;
 my @argbufp;
 my $versono;
 my $resorco;
+my %opthash = {};
+my @optlist = ();
 
 @argbuft = @ARGV;
 $versono = &getrg;
@@ -19,6 +21,36 @@ sub getrg {
   $lc_ret = "";
   if ( $lc_a > 0.5 ) { $lc_ret = shift(@argbuft); }
   return $lc_ret;
+}
+
+sub setopt {
+  @optlist = ( @optlist, $_[0] );
+  $opthash{$_[0]} = $_[1];
+}
+
+sub runopts {
+  my $lc_crg;
+  my $lc_ech;
+  my $lc_found;
+  my $lc_mth;
+  while ( &yet )
+  {
+    $lc_crg = &getrg;
+    $lc_found = 0;
+    foreach $lc_ech (@optlist)
+    {
+      if ( $lc_ech eq $lc_crg )
+      {
+        $lc_mth = $opthash{$lc_ech};
+        $lc_found = 10;
+      }
+    }
+    if ( $lc_found < 5 )
+    {
+      die "\n" . $_[0] . ": FATAL ERROR:\nUnknown Option: " . $lc_crg . ":\n\n";
+    }
+    &$lc_mth;
+  }
 }
 
 sub rset {
